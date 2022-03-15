60b1dccb8666c2a66b0b04a8848c4c55e929ad3d
yaerankim <77429312+yaerankim@users.noreply.github.com>
2022-03-16 00:44:59 +0900

    Update 게임_개발.py

--- CHAPTER04_Implementation/게임_개발.py
+++ CHAPTER04_Implementation/게임_개발.py
@@ -1,20 +1,29 @@
-# 68분/40분(아마 성공)
+# 68분/40분(애매한 성공, 제시된 test case만 실행해봄)
+# 3번째 메뉴얼인 '바라보는 방향 유지한 채 한 칸 뒤로 가고 1단계로 돌아가기' 코드X
 n, m = map(int,input().split())
 a, b, d = map(int,input().split())
+# gameMap 입력 고민만 15분
+# 교재 참고
+# gameMap = []
+# for i in range(n):
+#     gameMap.append(list(map(int, input().split())))
 gameMap = [i for i in range(n)]
 for i in range(n):
     gameMap[i] = list(map(int,input().split()))
 
 direction = [-1, 1, 1, -1]
 
-count = 1 # 이동한 칸의 수
-gameMap[a][b] = 1 # 이미 서 있는 곳 다시 방문 안 하도록 수정
+# 이동한 칸의 수
+count = 1
+# 현재 좌표 방문 처리
+gameMap[a][b] = 1
 rotation_count = 0
 new_d = d
 
-# 한 번의 칸 이동을 위한 탐색 반복
+# 칸 이동을 위한 탐색 반복
 while(True):
     # 왼쪽 방향으로 회전
+    # 교재에선 따로 turn_left()로 써서 사용
     if d == 0:
         d += 3
     else:
@@ -23,6 +32,8 @@ while(True):
     
     # 한 바퀴 다 돌았으면
     if rotation_count == 4:
+        # 교재 참고
+        # 다 회전한 뒤 뒤로 갈 수 있다면 이동하고 뒤가 바다로 막혀있다면 break
         break
 
     # 회전한 방향의 좌표로 임시 이동
@@ -34,6 +45,11 @@ while(True):
         new_b += direction[d]
 
     # 임시 이동한 곳이 육지인지 확인하고 실제로 이동
+    # 교재 참고
+    # 방문한 칸 저장용 배열(visited)과 육지와 바다를 표현한 전체 맵 표현용 배열(gameMap)을 따로 둠
+    # if visited[a][b] == 0 and gameMap[a][b] == 0:
+    # 회전한 이후 정면이 다 방문한 칸(visited == 1)이거나 바다(gameMap == 1)인 경우
+    # else:
     if gameMap[new_a][new_b] == 0:
         if d % 2 == 0:
             a += direction[d]
