476614fc3aec44f4d9b0be5b25c70e0e8be3b991
Yaeran Kim <yaerankim000812@gmail.com>
2023-09-10 14:41:38 +0900

    스택/큐 주식가격 문제풀이코드 수정 및 추가.

--- programmers/고득점kit/stack-queue/주식가격.py
+++ programmers/고득점kit/stack-queue/주식가격.py
@@ -1,15 +1,31 @@
 def solution(prices):
     answer = []
-    # 0) 정확성 6.7, 효율성 0.0, 6.7/100(30분)
-    for i in range(len(prices)):
+    # 1) 정확성 66.7, 효율성 33.3, 100/100(40분)
+    # "몇 초 후에 가격이 떨어지는가"에 초점을 두고 풀었을 때 해결
+    # 효율성 ~159.77ms, 19.5MB
+    for i in range(len(prices)-1):
         t = 0
         for j in range(i+1, len(prices)):
-            if i == len(prices) - 1:
-                break
             if prices[i] <= prices[j]:
                 t += 1
+            elif prices[i] > prices[j] and t != 0:
+                t += 1
+                break
             elif prices[i] > prices[j] and t == 0:
                 t += 1
                 break
         answer.append(t)
+    answer.append(0)
     return answer
+    # # 0) 정확성 6.7, 효율성 0.0, 6.7/100(30분)
+    # for i in range(len(prices)):
+    #     t = 0
+    #     for j in range(i+1, len(prices)):
+    #         if i == len(prices) - 1:
+    #             break
+    #         if prices[i] <= prices[j]:
+    #             t += 1
+    #         elif prices[i] > prices[j] and t == 0:
+    #             t += 1
+    #             break
+    #     answer.append(t)
